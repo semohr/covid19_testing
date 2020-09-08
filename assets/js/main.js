6db5c6a6e3e67a3e004ad16d57be399077dad4b4
Sebastian <sebastian@mohrenclan.de>
2020-09-08 13:45:27 +0200

    Added function to export modelData to csv

--- assets/js/main.js
+++ assets/js/main.js
@@ -549,4 +549,78 @@ function throttle(fn, interval) {
 }
 
 
+// Export to csv for validation
+function exportToCsv(filename, rows) {
+    var processRow = function (row) {
+        var finalVal = '';
+        for (var j = 0; j < row.length; j++) {
+          console.log(row[j],j)
+            var innerValue = row[j] === undefined ? '' : row[j].toString();
+            if (row[j] instanceof Date) {
+                innerValue = row[j].toLocaleString();
+            };
+            var result = innerValue.replace(/"/g, '""');
+            if (result.search(/("|,|\n)/g) >= 0)
+                result = '"' + result + '"';
+            if (j > 0)
+                finalVal += ',';
+            finalVal += result;
+        }
+        return finalVal + '\n';
+    };
 
+    var csvFile = '';
+    for (var i = 0; i < rows.length; i++) {
+        csvFile += processRow(rows[i]);
+    }
+
+    var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, filename);
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+}
+
+function modelData_to_csv(){
+  var data = [
+    ["time",
+    "T",
+    "T_A",
+    "T_S",
+    "H",
+    "H_A",
+    "H_S",
+    "N",
+    "N_obs",
+    "R_t_eff",
+    "R_t_obs"]];
+
+  for(var i=1; i<100; ++i){ 
+    data.push([
+      modelData["time"][i],
+      modelData["T"][i],
+      modelData["T_A"][i],
+      modelData["T_S"][i],
+      modelData["H"][i],
+      modelData["H_A"][i],
+      modelData["H_S"][i],
+      modelData["N"][i],
+      modelData["N_obs"][i],
+      modelData["R_t_eff"][i],
+      modelData["R_t_obs"][i]
+     ]);
+  }
+  console.log(data)
+  exportToCsv("data.csv", data)
+}
